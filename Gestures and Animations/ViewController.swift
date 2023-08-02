d7be112de4b0ac222c29558627b7fda26bf78cf3
Ajay Sarkate <ajusarkate2002@gmail.com>
2023-08-02 12:21:12 +0530

    Created a circle in view controller

--- Gestures and Animations/ViewController.swift
+++ Gestures and Animations/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let circle = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        view.addSubview(circle)
+        circle.backgroundColor = .systemGreen
+        
+        circle.translatesAutoresizingMaskIntoConstraints = false
+        circle.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        circle.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        circle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        circle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        
+        circle.layer.cornerRadius = 75
     }
 
 
